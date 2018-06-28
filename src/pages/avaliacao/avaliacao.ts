fc4d696ff759ff428b82175e2c927fc3e56a9977
Joao Vitor Moreno <jvmoreno99@gmail.com>
2018-06-28 17:59:46 -0300

    ultima ai Leo

--- src/pages/avaliacao/avaliacao.ts
+++ src/pages/avaliacao/avaliacao.ts
@@ -1,6 +1,6 @@
 import { Component,ViewChild } from '@angular/core';
 import {NavController, NavParams } from 'ionic-angular';
-import {ResultadoPage} from "../resultado/resultado";
+// import {ResultadoPage} from "../resultado/resultado";
 
 
 
@@ -28,6 +28,378 @@ export class AvaliacaoPage {
   idade = this.dataAtual - this.dataNascimento;
 @ViewChild('slider') slider: any;
   slides = [];
+  adultF = [
+    {
+      title: "Imagem 1",
+      description: "",
+      id: 1,
+      imc:"",
+      image: "assets/imgs/Adult_Mulher1.png",
+    },
+    {
+      title: "Imagem 2",
+      description: "",
+      id:2,
+      imc:"",
+      image: "assets/imgs/Adult_Mulher2.png",
+    },
+    {
+      title: "Imagem 3",
+      description: "",
+      id: 3,
+      imc:"",
+      image: "assets/imgs/Adult_Mulher3.png",
+    },
+    {
+      title: "Imagem 4",
+      description: "",
+      id: 4,
+      imc:"",
+      image: "assets/imgs/Adult_Mulher4.png",
+    },
+    {
+      title: "Imagem 5",
+      description: "",
+      id: 5,
+      imc:"",
+      image: "assets/imgs/Adult_Mulher5.png",
+    },
+    {
+      title: "Imagem 6",
+      description: "",
+      id: 6,
+      imc:"",
+      image: "assets/imgs/Adult_Mulher6.png",
+    },
+    {
+      title: "Imagem 7",
+      description: "",
+      id: 7,
+      imc:"",
+      image: "assets/imgs/Adult_Mulher7.png",
+    }, {
+      title: "Imagem 8",
+      description: "",
+      id: 8,
+      imc:"",
+      image: "assets/imgs/Adult_Mulher8.png",
+    },
+    {
+      title: "Imagem 9",
+      description: "",
+      id: 9,
+      imc:"",
+      image: "assets/imgs/Adult_Mulher9.png",
+    },
+    {
+      title: "Imagem 10",
+      description: "",
+      id: 10,
+      imc:"",
+      image: "assets/imgs/Adult_Mulher10.png",
+    },
+    {
+      title: "Imagem 11",
+      description: "",
+      id:11,
+      imc:"",
+      image: "assets/imgs/Adult_Mulher11.png",
+    },
+    {
+      title: "Imagem 12",
+      description: "",
+      id:12,
+      imc:"",
+      image: "assets/imgs/Adult_Mulher12.png",
+    },
+    {
+      title: "Imagem 13",
+      description: "",
+      id:13,
+      imc:"",
+      image: "assets/imgs/Adult_Mulher13.png",
+    },
+    {
+      title: "Imagem 14",
+      description: "",
+      id:14,
+      imc:"",
+      image: "assets/imgs/Adult_Mulher14.png",
+    },
+    {
+      title: "Imagem 15",
+      description: "",
+      id:15,
+      imc:"",
+      image: "assets/imgs/Adult_Mulher15.png",
+    },
+
+  ];
+
+  adultM =[ {
+    title: "Imagem 1",
+    description: "",
+    id: 1,
+    imc:"",
+    image: "assets/imgs/Adult_Homem1.png",
+  },
+    {
+      title: "Imagem 2",
+      description: "",
+      id:2,
+      imc:"",
+      image: "assets/imgs/Adult_Homem2.png",
+    },
+    {
+      title: "Imagem 3",
+      description: "",
+      id: 3,
+      imc:"",
+      image: "assets/imgs/Adult_Homem3.png",
+    },
+    {
+      title: "Imagem 4",
+      description: "",
+      id: 4,
+      imc:"",
+      image: "assets/imgs/Adult_Homem4.png",
+    },
+    {
+      title: "Imagem 5",
+      description: "",
+      id: 5,
+      imc:"",
+      image: "assets/imgs/Adult_Homem5.png",
+    },
+    {
+      title: "Imagem 6",
+      description: "",
+      id: 6,
+      imc:"",
+      image: "assets/imgs/Adult_Homem6.png",
+    },
+    {
+      title: "Imagem 7",
+      description: "",
+      id: 7,
+      imc:"",
+      image: "assets/imgs/Adult_Homem7.png",
+    }, {
+      title: "Imagem 8",
+      description: "",
+      id: 8,
+      imc:"",
+      image: "assets/imgs/Adult_Homem8.png",
+    },
+    {
+      title: "Imagem 9",
+      description: "",
+      id: 9,
+      imc:"",
+      image: "assets/imgs/Adult_Homem9.png",
+    },
+    {
+      title: "Imagem 10",
+      description: "",
+      id: 10,
+      imc:"",
+      image: "assets/imgs/Adult_Homem10.png",
+    },
+    {
+      title: "Imagem 11",
+      description: "",
+      id:11,
+      imc:"",
+      image: "assets/imgs/Adult_Homem11.png",
+    },
+    {
+      title: "Imagem 12",
+      description: "",
+      id:12,
+      imc:"",
+      image: "assets/imgs/Adult_Homem12.png",
+    },
+    {
+      title: "Imagem 13",
+      description: "",
+      id:13,
+      imc:"",
+      image: "assets/imgs/Adult_Homem13.png",
+    },
+    {
+      title: "Imagem 14",
+      description: "",
+      id:14,
+      imc:"",
+      image: "assets/imgs/Adult_Homem14.png",
+    },
+    {
+      title: "Imagem 15",
+      description: "",
+      id:15,
+      imc:"",
+      image: "assets/imgs/Adult_Homem15.png",
+    },
+
+  ];
+
+  kidM = [
+    {
+      title: "Imagem 1",
+      description: "",
+      id: 1,
+      imc:"",
+      image: "assets/imgs/Kid_Homem1.png",
+    },
+    {
+      title: "Imagem 2",
+      description: "",
+      id:2,
+      imc:"",
+      image: "assets/imgs/Kid_Homem2.png",
+    },
+    {
+      title: "Imagem 3",
+      description: "",
+      id: 3,
+      imc:"",
+      image: "assets/imgs/Kid_Homem3.png",
+    },
+    {
+      title: "Imagem 4",
+      description: "",
+      id: 4,
+      imc:"",
+      image: "assets/imgs/Kid_MHomem4.png",
+    },
+    {
+      title: "Imagem 5",
+      description: "",
+      id: 5,
+      imc:"",
+      image: "assets/imgs/Kid_Homem5.png",
+    },
+    {
+      title: "Imagem 6",
+      description: "",
+      id: 6,
+      imc:"",
+      image: "assets/imgs/Kid_Homem6.png",
+    },
+    {
+      title: "Imagem 7",
+      description: "",
+      id: 7,
+      imc:"",
+      image: "assets/imgs/Kid_Homem7.png",
+    }, {
+      title: "Imagem 8",
+      description: "",
+      id: 8,
+      imc:"",
+      image: "assets/imgs/Kid_Homem8.png",
+    },
+    {
+      title: "Imagem 9",
+      description: "",
+      id: 9,
+      imc:"",
+      image: "assets/imgs/Kid_Homem9.png",
+    },
+    {
+      title: "Imagem 10",
+      description: "",
+      id: 10,
+      imc:"",
+      image: "assets/imgs/Kid_Homem10.png",
+    },
+    {
+      title: "Imagem 11",
+      description: "",
+      id:11,
+      imc:"",
+      image: "assets/imgs/Kid_Homem11.png",
+    },
+  ];
+
+  kidF = [
+    {
+      title: "Imagem 1",
+      description: "",
+      id: 1,
+      imc:"",
+      image: "assets/imgs/Kid_Mulher1.png",
+    },
+    {
+      title: "Imagem 2",
+      description: "",
+      id:2,
+      imc:"",
+      image: "assets/imgs/Kid_Mulher2.png",
+    },
+    {
+      title: "Imagem 3",
+      description: "",
+      id: 3,
+      imc:"",
+      image: "assets/imgs/Kid_Mulher3.png",
+    },
+    {
+      title: "Imagem 4",
+      description: "",
+      id: 4,
+      imc:"",
+      image: "assets/imgs/Kid_Mulher4.png",
+    },
+    {
+      title: "Imagem 5",
+      description: "",
+      id: 5,
+      imc:"",
+      image: "assets/imgs/Kid_Mulher5.png",
+    },
+    {
+      title: "Imagem 6",
+      description: "",
+      id: 6,
+      imc:"",
+      image: "assets/imgs/Kid_Mulher6.png",
+    },
+    {
+      title: "Imagem 7",
+      description: "",
+      id: 7,
+      imc:"",
+      image: "assets/imgs/Kid_Mulher7.png",
+    }, {
+      title: "Imagem 8",
+      description: "",
+      id: 8,
+      imc:"",
+      image: "assets/imgs/Kid_Mulher8.png",
+    },
+    {
+      title: "Imagem 9",
+      description: "",
+      id: 9,
+      imc:"",
+      image: "assets/imgs/Kid_Mulher9.png",
+    },
+    {
+      title: "Imagem 10",
+      description: "",
+      id: 10,
+      imc:"",
+      image: "assets/imgs/Kid_Mulher10.png",
+    },
+    {
+      title: "Imagem 11",
+      description: "",
+      id:11,
+      imc:"",
+      image: "assets/imgs/Kid_Mulher11.png",
+    },
+  ];
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams) {
@@ -41,114 +413,23 @@ export class AvaliacaoPage {
     }
 
 showSlides() {
-    if(this.genero == 'femenino') {
-      if (this.idade > 13) {
-        this.slides = [{
-          title: "Imagem 1",
-          description: "",
-          id: 1,
-          imc: "",
-          image: "assets/imgs/Adult_Mulher1.png",
-        },
-          {
-            title: "Imagem 2",
-            description: "",
-            id: 2,
-            imc: "",
-            image: "assets/imgs/Adult_Mulher2.png",
-          },
-          {
-            title: "Imagem 3",
-            description: "",
-            id: 3,
-            imc: "",
-            image: "assets/imgs/Adult_Mulher3.png",
-          },
-          {
-            title: "Imagem 4",
-            description: "",
-            id: 4,
-            imc: "",
-            image: "assets/imgs/Adult_Mulher4.png",
-          },
-          {
-            title: "Imagem 5",
-            description: "",
-            id: 5,
-            imc: "",
-            image: "assets/imgs/Adult_Mulher5.png",
-          },
-          {
-            title: "Imagem 6",
-            description: "",
-            id: 6,
-            imc: "",
-            image: "assets/imgs/Adult_Mulher6.png",
-          },
-          {
-            title: "Imagem 7",
-            description: "",
-            id: 7,
-            imc: "",
-            image: "assets/imgs/Adult_Mulher7.png",
-          }, {
-            title: "Imagem 8",
-            description: "",
-            id: 8,
-            imc: "",
-            image: "assets/imgs/Adult_Mulher8.png",
-          },
-          {
-            title: "Imagem 9",
-            description: "",
-            id: 9,
-            imc: "",
-            image: "assets/imgs/Adult_Mulher9.png",
-          },
-          {
-            title: "Imagem 10",
-            description: "",
-            id: 10,
-            imc: "",
-            image: "assets/imgs/Adult_Mulher10.png",
-          },
-          {
-            title: "Imagem 11",
-            description: "",
-            id: 11,
-            imc: "",
-            image: "assets/imgs/Adult_Mulher11.png",
-          },
-          {
-            title: "Imagem 12",
-            description: "",
-            id: 12,
-            imc: "",
-            image: "assets/imgs/Adult_Mulher12.png",
-          },
-          {
-            title: "Imagem 13",
-            description: "",
-            id: 13,
-            imc: "",
-            image: "assets/imgs/Adult_Mulher13.png",
-          },
-          {
-            title: "Imagem 14",
-            description: "",
-            id: 14,
-            imc: "",
-            image: "assets/imgs/Adult_Mulher14.png",
-          },
-          {
-            title: "Imagem 15",
-            description: "",
-            id: 15,
-            imc: "",
-            image: "assets/imgs/Adult_Mulher15.png",
-          }];
-      }
+  if (this.genero == 'masculino') {
+    if (this.idade > 7) {
+      this.slides = this.kidM
     }
+    if (this.idade >= 13) {
+      this.slides = this.adultM;
+    }
+  }
+  else {
+    if (this.idade < 7) {
+      this.slides = this.kidF
+    }
+    if (this.idade >= 13) {
+      this.slides = this.adultF;
+    }
+
+  }
 }
   slideNext(){
     this.slider.slideNext();
@@ -158,19 +439,13 @@ showSlides() {
     this.slider.slidePrev();
   }
 
-  slidesSelecionados(slide){
-
-      this.addSlides(slide);
-
-    if(this.selecionado.length >= 3){
-      this.navCtrl.push(ResultadoPage,{imagens:this.selecionado});
-    }
-  }
 
   addSlides(slide) { //Método para adiconar o slide a lista de selecionado
+    if (this.selecionado.indexOf(slide) === -1) {
       this.selecionado.push(slide);
       console.log('selecionado');
       console.log(this.selecionado);
+    }
   }
   question() {
     while (this.selecionado.length <= 3) {
