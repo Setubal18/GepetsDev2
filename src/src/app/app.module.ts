9c3080105f0759d5107f1d15f05a14c6faace054
Joao Vitor Moreno <jvmoreno99@gmail.com>
2018-06-29 17:27:11 -0300

    n lembro oq eu fiz '-'

---
+++ src/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from "@angular/http";
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/storage';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { AddItemPage } from '../pages/add-item/add-item';
+import { ItemDetailPage } from '../pages/item-detail/item-detail';
+import { Data } from '../providers/data';
+import {AvaliacaoPage} from "../pages/avaliacao/avaliacao";
+import {ResultadoPage} from "../pages/resultado/resultado";
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    AddItemPage,
+    ItemDetailPage,
+    AvaliacaoPage,
+    ResultadoPage
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    AddItemPage,
+    ItemDetailPage,
+    AvaliacaoPage,
+    ResultadoPage
+
+  ],
+  providers: [Data, {provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
---
+++ src/src/pages/item-detail/item-detail.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NavParams, NavController} from 'ionic-angular';
+import {AvaliacaoPage} from "../avaliacao/avaliacao";
+
+@Component({
+  selector: 'page-item-detail',
+  templateUrl: 'item-detail.html'
+})
+export class ItemDetailPage {
+  altura;
+  nome;
+  dtnascimento;
+  genero;
+  peso
+  constructor(public navParams: NavParams,public navCtrl : NavController){
+
+  }
+
+  ionViewDidLoad() {
+    this.nome = this.navParams.get('item').nome;
+    this.dtnascimento = this.navParams.get('item').dtnascimento;
+    this.genero = this.navParams.get('item').genero;
+    this.peso = this.navParams.get('item').peso;
+    this.altura= this.navParams.get('item').altura;
+  }
+  chamadaAvaliacao(){
+    this.navCtrl.push(AvaliacaoPage,{paciente : this.navParams.get('item')});
+  }
+}
